d2fdf65cf508af2e617e7d7590a6e27de96b53fb
kevinj37 <kevinxhajiang@gmail.com>
2026-01-10 00:26:37 -0500

    Added some wrappers for communication

--- v1/Utility/Communication.java
+++ v1/Utility/Communication.java
@@ -1,4 +1,18 @@
 package v1.Utility;
 
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
 public class Communication {
+    public static void Write(RobotController rc, int idx, int message) throws GameActionException {
+        rc.writeSharedArray(idx, message);
+    }
+
+    public static void Read(RobotController rc, int idx) throws GameActionException {
+        rc.readSharedArray(idx);
+    }
+
+    public static int Encode(int x, int y) {
+        return x << 6 + y;
+    }
 }
